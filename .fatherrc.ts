8675c376a5209ead8bf46b1e8b33e2fbeb9146fd
arvinxx <arvinx@foxmail.com>
2023-01-08 20:07:29 +0800

    :wrench: build: 修正构建配置，兼容老的构建工具

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
-  cjs: { output: 'lib' },
+  cjs: {
+    output: 'lib',
+    transformer: 'babel',
+    // sketch 应该是内建了 node8 程度的 polyfill
+    targets: { node: 8 },
+  },
 });
